e5de1bc2897b71def2c656f1d58facad1aa9e19c
Rico Schiekel <fire@downgra.de>
2010-09-23 13:13:54 +0200

    fix up scalatest dependency and compiler options

--- project/build/Scopt.scala
+++ project/build/Scopt.scala
@@ -2,9 +2,10 @@ import sbt._
 
 class ScoptProject(info: ProjectInfo) extends DefaultProject(info) {
 
-  val newReleaseToolsRepository = "Scala Tools Repository" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
+  val scalaToolsSnapshots = ScalaToolsSnapshots
+
   val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
-  val junit = "junit" % "junit" % "4.7" % "test"
-  
-  val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
+
+  override def compileOptions = super.compileOptions ++ Seq(Unchecked)
+
 }
