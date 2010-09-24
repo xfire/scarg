698c7a4f9cf7fb8d998b5ad4cd0e42ecab82d33e
Rico Schiekel <fire@downgra.de>
2010-09-24 13:18:36 +0200

    change to new project structure with subprojects

---
+++ examples/src/main/scala/SimpleExample.scala
@@ -0,0 +1,16 @@
+package examples
+
+import org.github.scopt.ArgumentParser
+
+object SimpleExample {
+
+
+  class SimpleParser extends ArgumentParser
+
+  def main(args: Array[String]) {
+
+  }
+
+}
+
+// vim: set ts=2 sw=2 et:
--- project/build/Scopt.scala
+++ project/build/Scopt.scala
@@ -4,8 +4,45 @@ class ScoptProject(info: ProjectInfo) extends DefaultProject(info) {
 
   val scalaToolsSnapshots = ScalaToolsSnapshots
 
-  val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
-
+  // compile settings
   override def compileOptions = super.compileOptions ++ Seq(Unchecked)
+
+  // deploy/dist settings
+  lazy val distPath = info.projectPath / "dist"
+  def distName = "%s_%s-%s.zip".format(name, buildScalaVersion, version)
+
+  // Dependencies
+  object Dependencies {
+    lazy val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
+  }
+
+  // subprojects
+  lazy val core = project("core", "scopt-core", new CoreProject(_))
+  lazy val examples = project("examples", "scopt-examples", new ExamplesProject(_), core)
+
+  // scopt core subproject
+  class CoreProject(info: ProjectInfo) extends ScoptDefaultProject(info, distPath) {
+    val scalatest = Dependencies.scalatest
+  }
+
+  // examples subproject
+  class ExamplesProject(info: ProjectInfo) extends ScoptDefaultProject(info, distPath)
+
+  abstract class ScoptDefaultProject(info: ProjectInfo, val deployPath: Path) extends DefaultProject(info) {
+    lazy val dist = deployTask(jarPath, packageDocsJar, packageSrcJar, deployPath, true, true, true) dependsOn(
+      `package`, packageDocs, packageSrc) describedAs("Deploying")
+    def deployTask(jar: Path, docs: Path, src: Path, toDir: Path,
+                   genJar: Boolean, genDocs: Boolean, genSource: Boolean) = task {
+      def gen(jar: Path, toDir: Path, flag: Boolean, msg: String): Option[String] =
+      if (flag) {
+        log.info(msg + " " + jar)
+        FileUtilities.copyFile(jar, toDir / jar.name, log)
+      } else None
+
+      gen(jar, toDir, genJar, "Deploying bits") orElse
+      gen(docs, toDir, genDocs, "Deploying docs") orElse
+      gen(src, toDir, genSource, "Deploying sources")
+    }
+  }
 
 }
