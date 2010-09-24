969976f74af2d0ab8ccf1794ebc4f1460ba15ab4
Rico Schiekel <fire@downgra.de>
2010-09-24 22:01:06 +0200

    add two examples

---
+++ examples/src/main/scala/KeyValueExample.scala
@@ -0,0 +1,46 @@
+package examples
+
+import org.github.scopt.{ArgumentParser, ConfigMap, Reader}
+
+/*
+ * usage: KeyValueExample [options] 
+ * 
+ * options:
+ *   -k Key=Value   a key=value pair
+ *
+ */
+object KeyValueExample {
+
+  case class KeyValuePair(key: String, value: String)
+
+  class KeyValueConfig extends ConfigMap {
+    // type class to read an KeyValuePair
+    implicit object KeyValueReader extends Reader[KeyValuePair] {
+      def read(value: String): KeyValuePair = value.indexOf('=') match {
+          case n: Int if n >= 0 => 
+            val p = value.splitAt(n)
+            KeyValuePair(p._1, p._2)
+          case _ => throw new IllegalArgumentException("Expected a key=value pair")
+        }
+    }
+
+    lazy val pair = ("pair", KeyValuePair("", "")).as[KeyValuePair]
+  }
+
+  case class KeyValueParser(config: ConfigMap) extends ArgumentParser {
+    override val programName = Some("KeyValueExample")
+    
+    ! "-k" |^ "Key=Value" |% "a key=value pair" |> config.set("pair")
+  }
+
+  def main(args: Array[String]) {
+    val c = new KeyValueConfig
+
+    if(KeyValueParser(c).parse(args)) {
+      println("got key=%s and value=%s" format (c.pair.key, c.pair.value))
+    }
+  }
+
+}
+
+// vim: set ts=2 sw=2 et:
--- examples/src/main/scala/SimpleExample.scala
+++ examples/src/main/scala/SimpleExample.scala
@@ -1,14 +1,39 @@
 package examples
 
-import org.github.scopt.ArgumentParser
+import org.github.scopt.{ArgumentParser, ConfigMap}
 
+/**
+ * usage: SimpleExample [options] infile
+ * 
+ * options:
+ *   -v, --verbose   
+ *   -o OUT          
+ *   infile          
+ */
 object SimpleExample {
 
+  class Configuration extends ConfigMap {
+    lazy val verbose = get[Boolean]("verbose") getOrElse false
+    lazy val outfile = get[String]("outfile") getOrElse "-"
+    lazy val infile = ("infile", "").as[String]
+  }
 
-  class SimpleParser extends ArgumentParser
+  case class SimpleParser(config: ConfigMap) extends ArgumentParser {
+    override val programName = Some("SimpleExample")
+    
+    ! "-v" | "--verbose"   |% "active verbose output"            |> config.set("verbose")
+    ! "-o" |^ "OUT" |* "-" |% "output filename, default: stdout" |> { config.set("outfile", _) }
+    + "infile"             |% "input filename"                   |> config.set("infile")
+  }
 
   def main(args: Array[String]) {
+    val c = new Configuration
 
+    if(SimpleParser(c).parse(args)) {
+      println("verbose: " + c.verbose)
+      println("outfile: " + c.outfile)
+      println(" infile: " + c.infile)
+    }
   }
 
 }
