310a95fc6cbf75112e0f0100b7218d93815e5c64
Rico Schiekel <fire@downgra.de>
2010-11-14 19:40:04 +0100

    remove dist and add publishing via maven to gh-pages branch

--- project/build/Scarg.scala
+++ project/build/Scarg.scala
@@ -7,42 +7,19 @@ class ScargProject(info: ProjectInfo) extends DefaultProject(info) {
   // compile settings
   override def compileOptions = super.compileOptions ++ Seq(Unchecked)
 
-  // deploy/dist settings
-  lazy val distPath = info.projectPath / "dist"
-  def distName = "%s_%s-%s.zip".format(name, buildScalaVersion, version)
-
-  // Dependencies
-  object Dependencies {
-    lazy val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
-  }
+  // publish settings ("publish" to local gh-pages checkout)
+  override def managedStyle = ManagedStyle.Maven
+  lazy val publishTo = Resolver.file("github-pages-repo", new java.io.File("/home/fire/work/src/private/scala/scarg-gh-pages/maven-repo"))
 
   // subprojects
   lazy val core = project("core", "scarg-core", new CoreProject(_))
   lazy val examples = project("examples", "scarg-examples", new ExamplesProject(_), core)
 
   // scarg core subproject
-  class CoreProject(info: ProjectInfo) extends ScoptDefaultProject(info, distPath) {
-    val scalatest = Dependencies.scalatest
+  class CoreProject(info: ProjectInfo) extends DefaultProject(info) {
+    lazy val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
   }
 
   // examples subproject
-  class ExamplesProject(info: ProjectInfo) extends ScoptDefaultProject(info, distPath)
-
-  abstract class ScoptDefaultProject(info: ProjectInfo, val deployPath: Path) extends DefaultProject(info) {
-    lazy val dist = deployTask(jarPath, packageDocsJar, packageSrcJar, deployPath, true, true, true) dependsOn(
-      `package`, packageDocs, packageSrc) describedAs("Deploying")
-    def deployTask(jar: Path, docs: Path, src: Path, toDir: Path,
-                   genJar: Boolean, genDocs: Boolean, genSource: Boolean) = task {
-      def gen(jar: Path, toDir: Path, flag: Boolean, msg: String): Option[String] =
-      if (flag) {
-        log.info(msg + " " + jar)
-        FileUtilities.copyFile(jar, toDir / jar.name, log)
-      } else None
-
-      gen(jar, toDir, genJar, "Deploying bits") orElse
-      gen(docs, toDir, genDocs, "Deploying docs") orElse
-      gen(src, toDir, genSource, "Deploying sources")
-    }
-  }
-
+  class ExamplesProject(info: ProjectInfo) extends DefaultProject(info)
 }
